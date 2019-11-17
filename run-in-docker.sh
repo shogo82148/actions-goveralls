5c29cec434c00ea76cba5d17267f78a0a08afcc6
Ichinose Shogo <shogo82148@gmail.com>
2019-11-17 09:47:50 +0900

    add build script

---
+++ run-in-docker.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+CURRENT=$(cd "$(dirname "$0")" && pwd)
+docker run --rm -it \
+    -e GO111MODULE=on \
+    -e "GOOS=${GOOS:-linux}" -e "GOARCH=${GOARCH:-amd64}" -e "CGO_ENABLED=${CGO_ENABLED:-0}" \
+    -v "$CURRENT/.mod":/go/pkg/mod \
+    -v "$CURRENT":/go/src/github.com/shogo82148/actions-goveralls \
+    -w /go/src/github.com/shogo82148/actions-goveralls golang:1.13.4 "$@"
