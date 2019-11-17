80880e6b918ae97127438b2ef60d5499295877d2
Ichinose Shogo <shogo82148@gmail.com>
2019-11-17 15:54:41 +0900

    add code formatter

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
-import * as core from '@actions/core';
-import {wait} from './wait'
+import * as core from "@actions/core";
+import { wait } from "./wait";
 
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+    const ms = core.getInput("milliseconds");
+    console.log(`Waiting ${ms} milliseconds ...`);
 
-    core.debug((new Date()).toTimeString())
+    core.debug(new Date().toTimeString());
     await wait(parseInt(ms, 10));
-    core.debug((new Date()).toTimeString())
+    core.debug(new Date().toTimeString());
 
-    core.setOutput('time', new Date().toTimeString());
+    core.setOutput("time", new Date().toTimeString());
   } catch (error) {
     core.setFailed(error.message);
   }
