5d85a146959928a8ba23e945af75deec53ab2ea8
Ichinose Shogo <shogo82148@gmail.com>
2021-12-11 18:53:54 +0900

    bump Go v1.17.5

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -8,4 +8,4 @@ docker run --rm \
     -v actions-goveralls-cache:/go/pkg/mod \
     -v actions-goveralls-cache:/root/.cache \
     -v "$CURRENT":/go/src/github.com/shogo82148/actions-goveralls \
-    -w /go/src/github.com/shogo82148/actions-goveralls golang:1.17.2 "$@"
+    -w /go/src/github.com/shogo82148/actions-goveralls golang:1.17.5 "$@"
